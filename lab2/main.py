e0697255706310e9bab93b65e17e6a1d9e0188a8
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-08-20 23:08:19 +0530

    [ + ] Benchmark data addede

--- lab2/main.py
+++ lab2/main.py
@@ -173,7 +173,7 @@ class Solution:
 
 if __name__ == '__main__':
     sol = Solution()
-    sol.benchmark(5)
+    sol.benchmark(20)
     # sol.matrix = [[2,1,8],[3, 'B', 7],[6,5,4]]
     # sol.posx, sol.posy = 1, 1
     # sol.run()
