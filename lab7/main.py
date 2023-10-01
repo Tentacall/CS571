46f80068cd3a05da1a69a2003b49ec3e35a0d183
Sanskriti Singh <sanskriti180903@gmail.com>
2023-10-01 21:20:51 +0530

    Update main.py

--- lab7/main.py
+++ lab7/main.py
@@ -1,47 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from preprocessing import Utily
 
 class LinearRegression:
-    def __init__(self, data_x, data_y, epoch) -> None:
+    def __init__(self, data_x, data_y) -> None:
         self.data_x = data_x
         self.data_y = data_y
-        self.epox = epoch
         
-        # params 
-        self.theta1 = 1
-        self.theta2 = 0
-    
-
-
-    def fit(self):
-        for epoch in self.epox:
-            self._fit(self)
-
-    def _fit(self):
-        y_pred = self._predict(self.x)
-        loss = self.loss(y_pred)
-        self.gradient_decent(loss)
-
-    def _predict(self, x):
+        self.x_mean = np.mean(data_x)
+        self.y_mean = np.mean(data_y)
+        
+        self.x_var = np.var(data_x)
+        self.x_y_covar = np.cov(data_x, data_y)[0][1]
+        
+        self.theta1 = self.x_y_covar/self.x_var
+        self.theta2 = self.y_mean - self.theta1*self.x_mean
+        
+    def predict(self, x):
         return self.theta1*x + self.theta2
-    
-    # def predict(self, x):
-    #     return self.theta1*x + self.theta2
-    
-    def loss(self, predicted):
-        # return mean square error of all y values 
-        return np.mean((self.data_y - predicted)**2)
-
-    def gradient_decent(self, loss):
-        pass
-
-    def test(self):
-        pass
 
 
 if __name__ == '__main__':
-    x,y = Utily.load("data.tsv")
-    lr = LinearRegression(x,y, 10)
-    lr.fit()
-    lr.test()
+    import os
+    path = os.path.dirname(os.path.abspath(__file__)) + "/"
+    x,y = Utily.load(path + "data.tsv")
+    lr = LinearRegression(x,y)
+    print(lr.predict(100))
